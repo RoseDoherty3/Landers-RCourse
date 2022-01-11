9f64b1c53568ce34f2b6f13c5f2d721a61f60ae6
Rose Doherty <dohertyr993@gmail.com>
2022-01-11 14:55:30 +1030

    reviewing work with debriefing video

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Learning R with data camp
 this is a line from RStudio
 line added from GitHub
+testing new line 11/01/2022
+
+
--- module_2/R/week2R.R
+++ module_2/R/week2R.R
@@ -7,22 +7,43 @@
 
 #Import and Labeling
   #create a data frame called rt_df using the csv file
+  #what you want to call the new dataset in R <- read function(file location
+    #if the excel sheet/table has headers aready in place = TRUE so can
+      #be read by R)
 rt_df <- read.csv("module_2/data/week2.csv", header = TRUE)
 
   #Add labels to condition within rt_df: A should be labelled Control 
-    #and B should be labelled Exerimental
+    #and B should be labelled Experimental
+  #dataset name and specific variable of interest <- factor function(
+    #repeat names, current levels, new labels given)
 rt_df$condition <- factor(rt_df$condition, levels = c("A", "B"), 
                           labels = c("Control", "Experimental"))
+
+      ##alternate method according to debriefing (worked and changed levels 
+        #but didn't alter the variable information?)
+levels(rt_df$condition) <- factor(c("Control", "Experimental"))
+
   #add labels to gender: Male, Female and Transgender (for M, F, T)
+  #as before but with different variable and labels
 rt_df$gender <- factor(rt_df$gender, levels = c("M", "F", "T"), 
                        labels = c("Male", "Female", "Transgender"))
+
+      ##alternate method according to debriefing (worked and changed levels 
+        #but didn't alter the variable information?)
+levels(rt_df$gender) <- factor(c("Female", "Male", "Transgender"))
+
+#use to open dataset in R viewer
+View(rt_df)
 
 #Analysis
   #Display mean reaction time across rt_df using the mean() function.
 mean(rt_df$rt)
 
   #Create a new data frame called rt_f_df containing only female cases.
+  #use subset to grab one part of existing data frame, specify gender
 rt_f_df <- subset(rt_df, subset = gender == "Female")
+
+View(rt_f_df)
 
   #Using one command, display frequencies and/or quantitative summaries 
     #of all variables in rt_f_df
@@ -30,6 +51,7 @@ summary(rt_f_df)
 
   #Create a histogram of female reaction times by using the hist() 
     #function with one parameter: the rt column of rt_f_df
+  #save in figures folder as wel in plots window
 hist(rt_f_df$rt)
 
   #Create a list called datasets containing both rt_df and rt_f_df
@@ -37,4 +59,7 @@ datasets <- list(rt_df, rt_f_df)
 
   #Display the contents of the rt variable from the first dataset 
     #contained within datasets
+  #extract 1st dataset with double square brackets (single bracket would
+    #be subset showing 1st item in the list, double bracket is extraction
+      #on data from a list item)
 datasets[[1]][2]
